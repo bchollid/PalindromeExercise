41c9001c3f1ad5bf85c19afcfe079a8da5bf486e
bchollid <brandonholliday1@gmail.com>
2024-01-18 09:27:56 -0700

    Finished

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public WordSmith()
+		{
+		}
+
+		public bool IsAPalindrome(string word)
+		{
+            int length = word.Length;
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (word[i] != word[length - i - 1])
+                    return false;
+            }
+            return true;
+
+        }
+    }
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
 using Xunit;
 
-namespace PalindromeExerciseTests
+namespace PalindromeExercise.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("mom", true)]
+        [InlineData("civic", true)]
+        [InlineData("computer", false)]
+        [InlineData("video", false)]
+        [InlineData("keyboard", false)]
+        public void PalindromeTest(string word, bool expected)
         {
+            var newWordSmith = new WordSmith();
 
+            bool actual = newWordSmith.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
